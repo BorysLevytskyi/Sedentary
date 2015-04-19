3da5d275a1af25d793b5994786d9194a403405e8
Borys Levytskyi <blevtisky@yandex.ru>
2015-04-19 18:22:28 +0300

    Run only one version of the application

--- Sedentary/App.xaml.cs
+++ Sedentary/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows;
 using Autofac;
 using Caliburn.Micro;
@@ -9,11 +11,19 @@ using Sedentary.Model.Persistence;
 
 namespace Sedentary
 {
+
 	/// <summary>
 	///     Interaction logic for App.xaml
 	/// </summary>
 	public partial class App : Application
 	{
+		
+		[STAThread]
+		static void Main()
+		{
+			ApplicationStart.Start();
+		}
+
 		public App()
 		{
 			Trace.WriteLine(string.Empty);
@@ -46,7 +56,64 @@ namespace Sedentary
 			else
 			{
 				Tracer.Write("Unhandled error:\r\n{0}", e.ExceptionObject);
-			}			
+			}
+		}
+	}
+
+	public class SingleInstance : IDisposable
+	{
+		private Mutex _mutex;
+
+		public bool IsFirstRun()
+		{
+			bool newOneCreated;
+			_mutex = new Mutex(true, "SedenteryApplicationSingleInstanceKey", out newOneCreated);
+			return newOneCreated;
+		}
+
+		public void Dispose()
+		{
+			if (_mutex != null)
+			{
+				_mutex.ReleaseMutex();
+			}
+		}
+	}
+
+	public class ApplicationStart
+	{
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool SetForegroundWindow(IntPtr hWnd);
+
+		public static void Start()
+		{
+			using (var inst = new SingleInstance())
+			{
+				if (inst.IsFirstRun())
+				{
+					var app = new App();
+					app.InitializeComponent();
+					app.Run();
+				}
+				else
+				{
+					BringFrontExistingProcess();
+				}
+			}
+		}
+
+		private static void BringFrontExistingProcess()
+		{
+			Process current = Process.GetCurrentProcess();
+			foreach (Process process in Process.GetProcessesByName(current.ProcessName))
+			{
+				if (process.Id != current.Id)
+				{
+					SetForegroundWindow(process.MainWindowHandle);
+					break;
+				}
+			}
 		}
 	}
 }
